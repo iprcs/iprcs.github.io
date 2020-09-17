ce7ae2b2aa6111a83f029266940d51e3fceddde3
Irish Pattern Recognition and Classification Society <71457648+iprcs@users.noreply.github.com>
2020-09-17 18:21:21 +0100

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,111 @@
+<div id="sidebar">
+	<div class="inner">
+
+		<!-- Menu -->
+
+		<nav id="menu">
+			<header class="major">
+
+				<p>
+					<div class="12u$">
+						<span class="image left"> <img src="images/logo.gif" width="100%" alt="IPRCS" /></a></span>
+					</div>
+
+				</p>
+
+				<h2>Menu</h2>
+			</header>
+
+			<ul>
+				<li>
+					<a href="index.html">Welcome</a>
+
+				</li>
+				<li>
+					<a href="history.html">IPRCS history</a>
+
+				</li>
+
+				<li>
+					<span class="opener">IMVIP conferences</span>
+					<ul>
+
+						<li>
+							<a href="IMVIPconferences.html#edition">Upcoming IMVIP conference</a>
+						</li>
+
+						<li>
+							<a href="IMVIPconferences.html#IPRCSsupport">IPRCS Support</a>
+						</li>
+
+						<li>
+							<a href="IMVIPconferences.html#callOrganizer">Call for Organizers</a>
+						</li>
+
+						<li>
+							<a href="IMVIPconferences.html#sponsors">Call for Sponsors</a>
+						</li>
+
+					</ul>
+				</li>
+
+				<li>
+					<a href="IMVIP.html">IMVIP proceedings</a>
+				</li>
+	<li>
+					<a href="related.html">Related events &amp; societies</a>
+				</li>
+				
+
+			</ul>
+
+		</nav>
+
+		<!-- Section -->
+		<section>
+			<header class="major">
+				<h2>Get in touch</h2>
+			</header>
+
+			<ul class="contact">
+				<li class="fa-twitter">
+					<a href="https://twitter.com/iprcs"  target="_blank">@iprcs</a>
+				</li>
+				<li class="fa-linkedin">
+
+					<a  href="http://www.linkedin.com/groups/8402915"  target="_blank"><span class="label">LinkedIn</span></a>
+
+				</li>
+
+				<li class="fa-facebook" >
+					<a  href="https://www.facebook.com/iprcs.org/" target="_blank"><span class="label">Facebook</span></a>
+
+				</li>
+
+				<li class="fa-envelope-o">
+					<a href="mailto:Rozenn.Dahyot@tcd.ie">IPRCS president</a>
+				</li>
+
+				<li class="fa-home">
+					Ireland (ROI &amp; Northern Ireland)
+				</li>
+
+			</ul>
+
+		</section>
+
+		<!-- Search -->
+		<!-- 	<section id="search" class="alt">
+		<form method="post" action="#">
+		<input type="text" name="query" id="query" placeholder="Search" />
+		</form>
+		</section> -->
+		<!-- Footer -->
+		<footer id="footer">
+			<p class="copyright">
+				&copy; IPRCS
+
+			</p>
+		</footer>
+	</div>
+</div>
